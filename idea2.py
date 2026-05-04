8be734f2ea3fcc27d18b59ee8675b01e0e53a375
dddddddddddwad <ilimpro2013@gmail.com>
2026-05-04 15:54:36 +0300

    Create idea2.py

---
+++ idea2.py
@@ -0,0 +1,21 @@
+import discord, random, os, requests
+from discord.ext import commands
+import emoji
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command("duck")
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
