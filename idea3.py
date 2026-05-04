c9a9b906648f76ced2cebb5f07c5910d825a8a7d
dddddddddddwad <ilimpro2013@gmail.com>
2026-05-04 15:56:17 +0300

    Implement member join and dice roll commands
    Added commands to display member join date and roll dice.

---
+++ idea3.py
@@ -0,0 +1,26 @@
+import discord, random, os, requests
+from discord.ext import commands
+import emoji
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
