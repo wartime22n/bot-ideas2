7a34a6ac06ad37fb47037d79ba08307e7b980e82
dddddddddddwad <ilimpro2013@gmail.com>
2026-05-04 15:51:09 +0300

    Implement emoji feedback for user responses
    Added emoji responses for user feedback in bot commands.

--- bot2.py
+++ bot2.py
@@ -1,5 +1,6 @@
 import discord, random, os, requests
 from discord.ext import commands
+import emoji
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -75,11 +76,16 @@ async def rastgele(ctx):
     with open(f'recyle/{img_name2}', 'rb') as f:
         picture = discord.File(f)
     await ctx.send(file=picture)
-
+    await ctx.send('Did this help you?')
+    @bot.command()
+    async def no(ctx):
+        await ctx.send(emoji.emojize('i am sad hear that :disappointed_face:'))
+    @bot.command()
+    async def yes(ctx):
+        await ctx.send(emoji.emojize('i am glad to hear that! :smiling_face_with_smiling_eyes:'))
 @bot.command("duck")
 async def duck(ctx):
     '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
     image_url = get_duck_image_url()
     await ctx.send(image_url)
-
 bot.run("")
